83657f99d319a30638eb9be7936684277e0dd522
Salman Abuhaimed <85521119+BKSalman@users.noreply.github.com>
2025-02-02 21:33:14 +0300

    add sm_td

--- mQxqX/keymap.c
+++ mQxqX/keymap.c
@@ -3,20 +3,27 @@
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
+
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   HSV_0_255_255,
   HSV_74_255_255,
   HSV_169_255_255,
+  CKC_A,
+  CKC_S,
+  CKC_D,
+  CKC_F,
+  SMTD_KEYCODES_END,
 };
 
+#include "sm_td.h"
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
-    KC_ESCAPE,      MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LSFT, KC_D),MT(MOD_LCTL, KC_F),KC_G,                                           KC_H,           MT(MOD_RCTL, KC_J),MT(MOD_RSFT, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RGUI, KC_SCLN),KC_QUOTE,       
+    KC_ESCAPE,      CKC_A,          CKC_S,          CKC_D,          CKC_F,          KC_G,                                           KC_H,           MT(MOD_RCTL, KC_J),MT(MOD_RSFT, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RGUI, KC_SCLN),KC_QUOTE,       
     KC_LEFT_GUI,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,       
                                                     KC_DELETE,      LT(1,KC_SPACE),                                 LT(2,KC_ENTER), KC_BSPC
   ),
@@ -52,6 +59,10 @@ combo_t key_combos[COMBO_COUNT] = {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_smtd(keycode, record)) {
+      return false;
+  }
+
   switch (keycode) {
 
     case RGB_SLD:
@@ -82,4 +93,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-
+void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
+    switch (keycode) {
+        SMTD_MT(CKC_A, KC_A, KC_LEFT_GUI)
+        SMTD_MT(CKC_S, KC_S, KC_LEFT_ALT)
+        SMTD_MT(CKC_D, KC_D, KC_LSFT)
+        SMTD_MT(CKC_F, KC_F, KC_LEFT_CTRL)
+    }
+}
